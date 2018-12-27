f98134bd8b4d842611cc7c28afbfc2df2aed19d5
smdqromeo <1359247894@qq.com>
2018-12-27 21:34:20 +0800

    完成弹层

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -29,19 +29,40 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%" alt="">
     </div>
-    <div v-show="detailShow" class="detail">
+    <transition name="fade">
+      <div v-show="detailShow" class="detail" >
       <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
-          <div class="star-wrapper">
-            <star  :size="24" :score="3.7"></star>            
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star  :size="24" :score="3.7"></star>            
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="supports-item" v-for="(item,index) in seller.supports" :key="index">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
           </div>
         </div>
+        <div class="detail-close">
+          <i class="icon-close" @click="hideDetail"></i>
+        </div>
       </div>
-      <div class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 <script>
@@ -68,6 +89,9 @@ export default {
   methods:{
     showDetail(){
       this.detailShow = true
+    },
+    hideDetail(){
+      this.detailShow = false
     }
   }
 }
@@ -193,8 +217,7 @@ export default {
     width: 100%
     height: 100%
     overflow: auto 
-    background: rgba(7,1,27,.8)
-    
+    background: rgba(7,17,27,.8)
     .detail-wrapper
       min-height: 100%
       .detail-main
@@ -209,6 +232,56 @@ export default {
           margin-top: 18px
           padding: 2px 0
           text-align: center
+        .title
+          display:flex
+          width: 80%
+          margin: 28px auto 24px auto
+          .line
+            flex: 1
+            position:relative
+            top: -6px
+            border-bottom: 1px solid rgba(255,255,255,.2)
+          .text
+            padding: 0 12px
+            font-size: 14px
+            font-weight: 700
+        .supports
+          width: 80%
+          margin: 0 auto 
+          .supports-item
+            padding: 0 12px
+            margin-bottom: 12px
+            font-size: 0
+            &:last-child
+              margin-bottom: 0
+            .icon
+              display: inline-block
+              width: 16px
+              height: 16px
+              margin-right: 6px
+              vertical-align: top
+              background-size: 12px 12px
+              background-repeat: no-repeat
+              &.decrease
+                bg-image('decrease_2')
+              &.discount
+                bg-image('discount_2')
+              &.guarantee
+                bg-image('guarantee_2')
+              &.invoice
+                bg-image('invoice_2')
+              &.special
+                bg-image('special_2')
+            .text
+              font-size: 12px
+              line-height: 12px
+        .bulletin
+          width: 80%
+          margin: 0 auto 
+          .content
+            padding: 0 12px
+            font-size: 12px
+            line-height: 24px
     .detail-close     
       position: relative 
       width: 32px
@@ -216,4 +289,5 @@ export default {
       margin: -64px auto 0 auto 
       clear: both
       font-size: 32px
+    
 </style>
