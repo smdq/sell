68019083959fb77eac2a5a9460f85bf528de0292
smdqromeo <1359247894@qq.com>
2018-12-29 16:47:27 +0800

    完成good.vue中左右商品，有一个bug

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -1,12 +1,242 @@
 <template>
-  <div>GOODS</div>
+  <div class="goods">
+    <div class="menu-wrapper" ref="menu">
+      <ul>
+        <li class="menu-item border-bottom" @click="selectMenu(index,$event)"  :class="{'current':currentIndex ===index}" v-for="(item,index) in goods" :key="index">
+          <span class="text">
+            <span v-show="item.type>0" class="icon " :class="classMap[item.type]"></span>{{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper" ref="foods">
+      <ul>
+        <li class="food.list food-list-hook" v-for="(item,index) in goods" :key="index">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li class="food-item border-bottom" v-for="(food,index) in item.foods" :key="index">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon" alt="">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span>月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
+const ERR_OK = 0
 export default {
-  
+  props:{
+    seller:{
+      type:Object
+    }
+  },
+  data(){
+   return {
+     goods:[],
+     classMap:[],
+     listHeight:[],
+     scrollY:0
+   }
+  },
+  created(){
+    fetch('/api/goods')
+    .then(res => res.json())
+    .then(response =>{
+      if(response.errno == ERR_OK){
+        this.goods = response.data;
+        console.log(this.goods)
+
+        this.$nextTick( () =>{
+
+          this._initScroll()
+          this._calculateHeight()
+          
+        })
+      }
+    })
+
+    
+
+    this.classMap = ['decrease','discount','special','invoice','guarantee']
+  },
+  computed:{
+   currentIndex(){
+     for(let i =0;i<this.listHeight.length;i++){
+       let height1 = this.listHeight[i]
+       let height2 = this.listHeight[i+1]
+       if(!height2 || (this.scrollY >= height1 && this.scrollY < height2)){
+         return i
+       }
+     }
+     return 0
+   }
+   
+  },
+  methods:{
+    selectMenu(index,event){
+     
+     /* 当pcd端时event._constructed监听到时  不执行，当自己自定义派发是值为true才走下面index */
+     if(!event._constructed) {
+       return
+     }
+
+     let foodList = this.$refs.foods.getElementsByClassName('food-list-hook')
+     let el = foodList[index]
+     this.foodsScroll.scrollToElement(el)
+      //console.log(index)
+      
+     
+    },
+    _initScroll(){
+
+      this.menuScroll = new BScroll(this.$refs.menu,{click:true})
+
+      this.foodsScroll = new BScroll(this.$refs.foods,{
+        // 实时监听滚动位置参数
+        probeType: 3,
+        click:true
+      })
+
+      this.foodsScroll.on('scroll',(pos) =>{
+        this.scrollY = Math.abs(Math.round(pos.y))
+        console.log(this.scrollY)
+      })
+    },
+
+    _calculateHeight(){
+      let foodList = this.$refs.foods.getElementsByClassName('food-list-hook')
+
+      let height = 0;
+      this.listHeight.push(height)
+      for(let i=0;i<foodList.length;i++){
+        let item = foodList[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
+      //console.log(this.listHeight)
+    }
+  }
 }
 </script>
-<style>
+<style lang="stylus" scoped>
 
+@import '~common/stylus/varibel.styl'
+ .goods
+   display: flex
+   position: absolute 
+   width: 100%
+   top: 174px
+   bottom: 46px
+   overflow:hidden
+   .menu-wrapper
+     flex: 0 0 80px
+     width: 80px
+     background: #f3f5f7
+     .menu-item
+       display: table
+       height: 54px
+       width: 56px
+       line-height: 14px
+       padding: 0 12px
+       &.current
+        position: relative
+        z-index: 10
+        margin-top: -1px
+        background: #ffffff
+        font-weight: 700
+        .text
+          border:none
+       .icon
+         display: inline-block
+         width: 12px
+         height: 12px
+         margin-right: 2px
+         background-size: 12px 12px
+         background-repeat: no-repeat
+         vertical-align: top
+         &.decrease
+            bg-image('decrease_3')
+          &.discount
+            bg-image('discount_3')
+          &.guarantee
+            bg-image('guarantee_3')
+          &.invoice
+            bg-image('invoice_3')
+          &.special
+            bg-image('special_3')
+       .text
+         display: table-cell
+         width: 56px
+         font-size: 12px
+         vertical-align: middle
+         font-size: 12px
+   .foods-wrapper
+     flex: 1
+     .title
+       padding-left: 14px
+       height: 26px
+       line-height: 26px
+       border-left: 2px solid #d9dde1
+       font-size: 12px
+       color: rgb(147,153,159)
+       background:#f3f5f7
+     .food-item
+       display: flex
+       margin: 18px
+       padding-bottom: 18px
+       border-top: none
+       &:last-child
+         border:none
+         margin-bottom: 0
+       .icon
+         flex: 0 0 57px
+         margin-right: 10px
+       .content
+         flex: 1
+         .name
+           margin: 2px 0 8px 0
+           font-sisez: 14px
+           line-height: 14px
+           font-size: 14px
+           color: rgb(7,17,27)
+         .desc
+           margin-bottom: 8px
+           line-height:  12px
+           font-size: 10px
+           color:rgb(147,153,159)
+         .extra
+           line-height: 10px
+           font-size: 10px
+           color:rgb(147,153,159)
+           .count
+             margin-right: 12px
+         .price
+           font-weigth: 700
+           line-height: 24px
+           .now
+             margin-right: 18px
+             font-size: 14px
+             color: rgb(240,20,20)
+           .old
+             text-decoration: line-through
+             font-size: 10px
+             color:rgb(147,153,159)
+           
 </style>
 
