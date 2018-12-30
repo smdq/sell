25ee7b0a290824e341fe4b1f41c84e33b192737f
smdqromeo <1359247894@qq.com>
2018-12-30 22:48:04 +0800

    完成小球的抛物线运动

--- sell/src/components/shopcart/shopcart.vue
+++ sell/src/components/shopcart/shopcart.vue
@@ -18,16 +18,18 @@
       </div>
      
     </div>
-     <div class="ball-container">
-        <div class="ball"  v-for="(ball,index) in balls" :key="index" v-show="ball.show">
-             
-         <transition @before-enter="beforeEnter" @enter="enter" @after-enter= "afterEnter">
-          
-           
-           <div class="inner inner-hook"></div>
-         </transition>
-         </div>
+    <div class="ball-container">
+      <div v-for="(ball,index) in balls" :key="index">
+      <!-- //用了两种方式的动画,css和js钩子 -->
+        <transition name="drop" @before-enter="beforeDrop" @enter="dropping" @after-enter="afterDrop">
+        <!-- //外层动画 -->
+          <div class="ball" v-show="ball.show">
+          <!-- 内层动画 -->
+            <div class="inner inner-hook"></div>
+          </div>
+        </transition>
       </div>
+    </div>
   </div>
 </template>
 <script>
@@ -106,54 +108,61 @@ export default {
       }
     }
   },
-  methods:{
-    drop(el){
-      console.log(el)
-      for(let  i=0;i<this.balls.length;i++){
-        let ball = this.balls[i];
-        if(!ball.show) {
-          ball.show=true;
-          ball.el = el
-          this.dropBalls.push(ball);
-          return;
+  methods: {
+      drop(el) { 
+      //触发一次事件就会将所有小球进行遍历
+        for (let i = 0; i < this.balls.length; i++) {
+          let ball = this.balls[i];
+          if (!ball.show) { //将false的小球放到dropBalls
+            ball.show = true;
+            ball.el = el; //设置小球的el属性为一个dom对象
+            this.dropBalls.push(ball); 
+            return;
+          }
+        }
+      },
+
+      beforeDrop(el){ //这个方法的执行是因为这是一个vue的监听事件
+        let count = this.balls.length;
+        while (count--) {
+          let ball = this.balls[count];
+          if (ball.show) {
+            let rect = ball.el.getBoundingClientRect(); //获取小球的相对于视口的位移(小球高度)
+            let x = rect.left - 32;
+            let y = -(window.innerHeight - rect.top - 22); //负数,因为是从左上角往下的的方向
+            el.style.display = ''; //清空display
+            el.style.webkitTransform = `translate3d(0,${y}px,0)`; 
+            el.style.transform = `translate3d(0,${y}px,0)`;
+            //处理内层动画
+            let inner = el.getElementsByClassName('inner-hook')[0]; //使用inner-hook类来单纯被js操作
+            inner.style.webkitTransform = `translate3d(${x}px,0,0)`;
+            inner.style.transform = `translate3d(${x}px,0,0)`;
+          }
+        }
+      },
+
+      dropping(el, done) { //这个方法的执行是因为这是一个vue的监听事件
+        /* eslint-disable no-unused-vars */
+        let rf = el.offsetHeight; //触发重绘html
+        this.$nextTick(() => { //让动画效果异步执行,提高性能
+          el.style.webkitTransform = 'translate3d(0,0,0)';
+          el.style.transform = 'translate3d(0,0,0)';
+          //处理内层动画
+          let inner = el.getElementsByClassName('inner-hook')[0]; //使用inner-hook类来单纯被js操作
+          inner.style.webkitTransform = 'translate3d(0,0,0)';
+          inner.style.transform = 'translate3d(0,0,0)';
+          el.addEventListener('transitionend', done); //Vue为了知道过渡的完成，必须设置相应的事件监听器。
+        });
+      },
+
+      afterDrop(el) { //这个方法的执行是因为这是一个vue的监听事件
+        let ball = this.dropBalls.shift(); //完成一次动画就删除一个dropBalls的小球
+        if (ball) {
+          ball.show = false;
+          el.style.display = 'none'; //隐藏小球
         }
       }
-    },
-    beforeEnter(el){
-      let count = this.balls.length;
-       while( count--){
-         let ball = this.balls[count];
-         if(ball.show){
-           let rect = ball.el.getBoundingClientRect();
-           let x = rect.left -32;
-           let y =-(window.innerHeight-rect.top-22);
-           el.style.display = '';
-           el.style.webKitTransform = `translate3d(0,${y}px,0)`;
-           el.style.transform = `translate3d(0,${y}px,0)`;
-           let inner = el.getElementsByClassName('inner-hook')[0]
-           inner.style.webKitTransform = `translate3d(${x}px,0,0)`;
-           inner.style.transform = `translate3d(${x}px,0,0)`;
-         }
-       }
-    },
-    enter(el){
-      let rf = el.offsetHeight;
-       this.$nextTick(()=>{
-         el.style.webKitTransform = `translate3d(0,${y}px,0)`;
-           el.style.transform = `translate3d(0,${y}px,0)`;
-           let inner = el.getElementsByClassName('inner-hook')[0]
-           inner.style.webKitTransform = `translate3d(${x}px,0,0)`;
-           inner.style.transform = `translate3d(${x}px,0,0)`;
-       })
-    },
-    afterEnter(el){
-      let ball = this.dropBalls.shift()
-       if(ball){
-         ball.show =false;
-         el.style.display = 'none';
-       }
     }
-  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -252,18 +261,17 @@ export default {
             color: #ffffff
     .ball-container
       
-      .ball 
-        position: fixed
+      .ball
+        position: fixed 
         left: 32px
         bottom: 22px
-        z-index: 200     
-        &.drop-transition
-          transition: all 0.4s
-          .inner 
-            height: 16px
-            width: 16px
-            border-radius: 50%
-            background: rgb(0,160,120)
-            transition: all 0.4s 
+        z-index: 200 
+        transition: all 0.4s cubic-bezier(0.49, -0.29, 0.75, 0.41)
+        .inner
+          width: 16px
+          height: 16px
+          border-radius: 50%
+          background: rgb(0, 160, 220)
+          transition: all 0.4s linear
 </style>
 
